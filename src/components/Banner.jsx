ef358e924d2579b11751fd3d325d44a5bd782334
codermaccoil <codermaccoil@gmail.com>
2024-01-25 11:58:28 +0100

    "add shadow to title"

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,8 +7,8 @@ const Banner = () => {
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ ease: "easeInOut", duration: 2.9, delay: 0.2 }}
-        className="flex justify-center">
-        <h1 className="mt-24 text-8xl text-secondary-dark">
+        className="flex justify-center ">
+        <h1 className="title-name mt-24 text-8xl text-secondary-dark">
           Oisín McCole
         </h1>
       </motion.div>
