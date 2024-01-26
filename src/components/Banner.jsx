a60ecd7a5bd8b4f3c121d0ae4291eb0c3c13f36e
codermaccoil <codermaccoil@gmail.com>
2024-01-26 09:21:04 +0100

    "add meta tags and change animation"

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,15 +3,15 @@ import { motion } from "framer-motion";
 const Banner = () => {
   return (
     <>
-      <motion.div
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ ease: "easeInOut", duration: 2.9, delay: 0.2 }}
-        className="banner-container border-8 border-secondary-dark bg-primary-dark">
-        <h1 className="title-name text-8xl text-secondary-dark ">
+      <div className="banner-container border-8 border-secondary-dark bg-primary-dark">
+        <motion.h1
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ ease: "easeInOut", duration: 2.9, delay: 0.2 }}
+          className="title-name text-8xl text-secondary-dark ">
           Oisín McCole
-        </h1>
-      </motion.div>
+        </motion.h1>
+      </div>
     </>
   );
 };
