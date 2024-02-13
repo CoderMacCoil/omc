7a4a93ad6d9c8f7f6ec7125ac59e926172793c18
codermaccoil <codermaccoil@gmail.com>
2024-02-13 14:43:31 +0100

    "add rose and henry"

--- src/components/BirthdayGreeting.jsx
+++ src/components/BirthdayGreeting.jsx
@@ -3,17 +3,21 @@ import { motion } from "framer-motion";
 function BirthdayGreeting() {
   return (
     <motion.div
-      initial={{ opacity: 0}}
+      initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ ease: "easeInOut", duration: 2.5, delay: 1 }}
       className="mt-8 text-center md:text-left md:ms-8 text-2xl text-secondary-dark drop-shadow">
       <div className="text md:flex">
-        <p className="text-pimary-light">Dear brother,</p>
+        <p className="text-pimary-light">Dear brother / uncle,</p>
         <p>you are beautiful.</p>
         <p>Bíodh lá lovely agat.</p>
       </div>
       <p>Grá,</p>
-      <img src="/fi.svg" alt="Fi signature" className="md:w-48 mt-2" />
+      <div className="signatures flex justify-center">
+        <img src="/fi.svg" alt="Fi signature" className="w-24 md:w-48 mt-2" />
+        <img src="/rose.svg" alt="Rose signature" className="w-24 md:w-48 mt-2" />
+        <img src="/henry.svg" alt="Henry signature" className="w-24 md:w-48 mt-2" />
+      </div>
     </motion.div>
   );
 }
