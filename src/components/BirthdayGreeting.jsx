e46dfa124de64d38efa19989f3fd3bc54fda6566
codermaccoil <codermaccoil@gmail.com>
2024-02-13 09:13:31 +0100

    "add animation to BirthdayGreeting"

--- src/components/BirthdayGreeting.jsx
+++ src/components/BirthdayGreeting.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import { motion } from "framer-motion";
 
 function BirthdayGreeting() {
   return (
-    <div className="mt-8 text-center md:text-left md:ms-8 text-2xl text-secondary-dark drop-shadow">
+    <motion.div
+      initial={{ opacity: 0}}
+      animate={{ opacity: 1 }}
+      transition={{ ease: "easeInOut", duration: 2.5, delay: 1 }}
+      className="mt-8 text-center md:text-left md:ms-8 text-2xl text-secondary-dark drop-shadow">
       <div className="text md:flex">
         <p className="text-pimary-light">Dear brother,</p>
         <p>you are beautiful.</p>
         <p>Bíodh lá lovely agat.</p>
-        
       </div>
       <p>Grá,</p>
       <img src="/fi.svg" alt="Fi signature" className="md:w-48 mt-2" />
-    </div>
+    </motion.div>
   );
 }
 
