0bb740f986c36a6235fbfffce849df58ca10b2a6
codermaccoil <codermaccoil@gmail.com>
2024-02-14 08:15:25 +0100

    "remove BirthdayGreeting"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Banner from "./components/Banner";
 import SocialMediaLinks from "./components/SocialMediaLinks";
-import BirthdayGreeting from "./components/BirthdayGreeting";
+
 
 function App() {
   return (
     <div className="app border-16 rounded-lg border-secondary-dark bg-primary-dark h-svh">
       <Banner />
       <SocialMediaLinks />
-      <BirthdayGreeting />
     </div>
   );
 }
